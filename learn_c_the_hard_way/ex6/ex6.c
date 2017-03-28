c38f6ce27e7b1bc466958485432cb7c7a5d77209
adamzfc <adamzfc@gmail.com>
2017-03-28 23:43:14 +0800

    update learn c the hard way ex

--- learn_c_the_hard_way/ex6/ex6.c
+++ learn_c_the_hard_way/ex6/ex6.c
@@ -14,5 +14,9 @@ int main(int argc, char *argv[])
     printf("You have %f awesome super powers.\n", super_power);
     printf("I have an initial %c.\n", initial);
     printf("I have a first name %s.\n", first_name);
-    
+    printf("I have a last name %s.\n", last_name);
+    printf("My whole name is %s %c. %s.\n",
+            first_name, initial, last_name);
+
+    return 0;
 }
