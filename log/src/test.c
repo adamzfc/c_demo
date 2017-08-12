3b96901eda1bf6141a740974001a6c824331fd7e
adamzfc <adamzfc@gmail.com>
2017-08-12 16:05:12 +0800

    update log

--- log/src/test.c
+++ log/src/test.c
@@ -1,11 +1,14 @@
 #include "log.h"
+#define UNUSED(expr) do { (void)(expr); } while(0)
 
 int main(int argv, char** argc) {
+    UNUSED(argv);
+    UNUSED(argc);
     log_set_enable(1);
     log_set_level(LOG_DEBUG);
-    log_debug("Hello world");
-    log_info("Hello world");
-    log_warn("Hello world");
-    log_error("Hello world");
+    log_debug("Hello World");
+    log_info("Hello World");
+    log_warn("Hello World");
+    log_error("Hello World");
     return 0;
 }
